fd8444f18e0cad71d76bcaf56184ca139ceba27e
ngot23 <levuquang2k1@gmail.com>
2020-09-30 00:39:41 +0700

    Init

---
+++ src/Controller/DictionaryApplication.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DictionaryApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML_File/MainController.fxml"));
+        primaryStage.setTitle("En - Vi Dictionary");
+        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.show();
+    }
+}
---
+++ src/Controller/MainController.java
@@ -0,0 +1,55 @@
+package Controller;
+
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class MainController {
+    @FXML
+    private ResourceBundle resources;
+    @FXML
+    private URL location;
+//    @FXML
+//    private Button editButton;
+//    @FXML
+//    private Button addButton;
+//    @FXML
+//    private Button homeButton;
+//    @FXML
+//    private Button googleButton;
+//
+//    @FXML
+//    private AnchorPane contentPane;
+//
+//    private AnchorPane homeAnchorPane;
+//    private AnchorPane currentAnchorPane;
+//    private homeController home_controller;
+//
+//    public void setContentPane(AnchorPane anchorPane) {
+//        this.contentPane.getChildren().setAll(anchorPane);
+//        this.currentAnchorPane = anchorPane;
+//    }
+//
+//    @FXML
+//    public void showHomePane() {
+//        this.setContentPane(homeAnchorPane);
+//        home_controller.Run();
+//    }
+//
+//    @FXML
+//    public void chooseOption(ActionEvent event) {
+//        if (event.getSource() == homeButton) {
+//            showHomePane();
+//        }
+//    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
